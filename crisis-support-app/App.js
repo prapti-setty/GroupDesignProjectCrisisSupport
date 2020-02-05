e1a4d14dd05db0cbbea51efd69afd98b605d3e5f
prapti-setty <settyp@tcd.ie>
2020-02-05 10:42:16 +0000

    changed the icon to the trocaire icon& added opening screen

--- crisis-support-app/App.js
+++ crisis-support-app/App.js
@@ -1,9 +1,10 @@
 import { AppLoading } from 'expo';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import SplashScreen from 'react-native-splash-screen'
 
 import AppNavigator from './navigation/AppNavigator';
 
