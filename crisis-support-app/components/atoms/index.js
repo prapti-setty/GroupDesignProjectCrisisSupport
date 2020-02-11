8fe13a9f3f1bdacfeba10d11761944662e57966a
ConorClery <cleryc@tcd.ie>
2020-02-11 15:57:23 +0000

    Made atoms accessible globally via index.js exports

---
+++ crisis-support-app/components/atoms/index.js
@@ -0,0 +1,4 @@
+export {default as NavigationButton} from './NavigationButton.js';
+export {default as PinEntry} from './PinEntry.js';
+export {default as MonoText} from './StyledText.js';
+export {default as TabBarIcon} from './TabBarIcon.js';
