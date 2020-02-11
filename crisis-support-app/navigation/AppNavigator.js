866363ea4c949bd61ad6ab6ba993cef70732e384
ConorClery <cleryc@tcd.ie>
2020-02-11 13:11:47 +0000

    Expanding main navigation handler

--- crisis-support-app/navigation/AppNavigator.js
+++ crisis-support-app/navigation/AppNavigator.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+import MainNavigator from './MainNavigator';
 
 export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Main: MainTabNavigator,
-  })
+    MainTab: MainTabNavigator,
+    Main: MainNavigator,
+  },
+  {
+    initialRouteName: Main,
+  },
+)
 );
