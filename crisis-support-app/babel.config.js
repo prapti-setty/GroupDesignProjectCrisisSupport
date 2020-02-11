8b495a54ff42f705253d4aa7ef0d82c54f1d3160
ConorClery <cleryc@tcd.ie>
2020-02-11 15:54:52 +0000

    Configuring import routes/aliases

--- crisis-support-app/babel.config.js
+++ crisis-support-app/babel.config.js
@@ -2,5 +2,25 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          cwd: "babelrc",
+          root: ["./"],
+          extensions: [".js", ".ios.js", ".android.js"],
+          alias: {
+            _assets: "./assets",
+            _components: "./components",
+            _atoms: "./components/atoms",
+            _molecules: "./components/molecules",
+            _organisms: "./components/organisms",
+            _navigation: "./navigation",
+            _screens: "./screens",
+            _constants: "./constants",
+          }
+        }
+      ]
+    ]
   };
 };
