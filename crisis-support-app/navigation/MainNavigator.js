a494463760a73c1ae979eb53254a6e254a93bb65
ConorClery <cleryc@tcd.ie>
2020-02-11 22:11:22 +0000

    Created main navigator to temporarily replace tab navigator

--- crisis-support-app/navigation/MainNavigator.js
+++ crisis-support-app/navigation/MainNavigator.js
@@ -2,22 +2,55 @@ import React from 'react';
 import { Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import SettingsScreen from '../screens/SettingsScreen';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import FormTest from '../screens/FormTest';
+
 
 const config = Platform.select({
   web: { headerMode: 'screen' },
   default: {},
 });
 
+const Home = {
+  screen: HomeScreen,
+  path: '',
+  navigationOptions: ({navigation}) => ({
+    title: 'Home'
+  }),
+};
+
+const Settings = {
+  screen: SettingsScreen,
+  path: '',
+  navigationOptions: ({navigation}) => ({
+    title: 'Settings',
+  }),
+};
+
+const Links = {
+  screen: LinksScreen,
+  path: '',
+  navigationOptions: ({navigation}) => ({
+    title: 'Links',
+  }),
+};
+
+const Forms = {
+  screen: FormTest,
+  path: '',
+  navigationOptions: ({navigation}) => ({
+    title: 'Forms',
+  }),
+};
+
 
 const mainNavigator = createStackNavigator({
-    Settings: {
-      screen: SettingsScreen,
-      path: '',
-      navigationOptions: ({navigation}) => ({
-        title: 'Settings'
-      }),
-    },
-  },
+    Home: Home,
+    Settings: Settings,
+    Links: Links,
+    Forms: Forms,
+  }, {initialRouteName: 'Home'},
 );
 
 export default mainNavigator;
