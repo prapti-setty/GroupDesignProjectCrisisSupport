d416aebd24c997dd80a5978da5d18cc42aeee0cd
ConorClery <cleryc@tcd.ie>
2020-02-24 17:07:45 +0000

    Export default screens for use in '_screens' alias

---
+++ crisis-support-app/screens/index.js
@@ -0,0 +1,9 @@
+export { default as AdminPin } from './AdminPin.js';
+export { default as AdminScreen } from './AdminScreen.js';
+export { default as FormTest } from './FormTest.js';
+export { default as HomeScreen } from './HomeScreen.js';
+// export { default as JsonForm } from './JsonForm.js';
+export { default as LinksScreen } from './LinksScreen.js';
+export { default as OpeningScreen } from './OpeningScreen.js';
+export { default as SettingsScreen } from './SettingsScreen.js';
+export { default as VisitorScreen } from './VisitorScreen.js';
