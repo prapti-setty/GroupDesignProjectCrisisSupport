988ec739371e815ff569ca0ab96ab8d426387af9
ConorClery <cleryc@tcd.ie>
2020-02-24 17:09:48 +0000

    Cleaned PinNavigator screen imports

--- crisis-support-app/navigation/PinNavigator.js
+++ crisis-support-app/navigation/PinNavigator.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
-import AdminPin from '../screens/AdminPin';
-import AdminScreen from '../screens/AdminScreen';
-
+import { AdminPin, AdminScreen } from '_screens';
 
 const config = Platform.select({
   web: { headerMode: 'screen' },
