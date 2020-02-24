aa29f2df6ca26ab788d705d7f3f9236f3a17b825
ConorClery <cleryc@tcd.ie>
2020-02-24 17:08:35 +0000

    Cleaned MainTabNavigator screen imports

--- crisis-support-app/navigation/MainTabNavigator.js
+++ crisis-support-app/navigation/MainTabNavigator.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-
-import TabBarIcon from '../components/atoms/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import FormTest from '../screens/FormTest';
-import SettingsScreen from '../screens/SettingsScreen';
+import { TabBarIcon } from '_atoms';
+import { HomeScreen, LinksScreen, FormTest, SettingsScreen } from '_screens';
 
 const config = Platform.select({
   web: { headerMode: 'screen' },
