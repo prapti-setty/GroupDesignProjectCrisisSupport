cf30df878a8bd61e194c7c75316ca6538825081f
ConorClery <cleryc@tcd.ie>
2020-02-24 17:44:31 +0000

    Created complaint form template file

---
+++ crisis-support-app/screens/visitorScreens/ComplaintForm.js
@@ -0,0 +1,20 @@
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  SafeAreaView,
+} from 'react-native';
+
+export default function ComplaintForm({ navigation }) {
+  return (
+    <View>
+
+    </View>
+  );
+}
