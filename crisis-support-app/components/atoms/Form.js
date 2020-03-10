070a89cc8ba3265eea4fccf0412a11cd2534e8d1
Cathal Kinsella <kinselc3@tcd.ie>
2020-03-10 11:58:39 +0000

    Forms to email output

--- crisis-support-app/components/atoms/Form.js
+++ crisis-support-app/components/atoms/Form.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import { View,ScrollView, Text, StyleSheet, TextInput, Button,Picker} from "react-native";
+import { View,ScrollView, Text, StyleSheet, TextInput, Button, Alert,Picker} from "react-native";
 import { Colors } from '_constants';
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import email from 'react-native-email'
 export default class Form extends Component{
 
     constructor(props) {
@@ -11,21 +12,64 @@ export default class Form extends Component{
           output: [],
           _update:"",
         }
+        this.subject = props.title;
+        this.recipient ="figawaf815@mrisemail.com";
         this.updateAnswer = (answer,index) => {
           const newArray = [...this.state.output];
           newArray[index] = answer;
           this.setState({ output: newArray });
-          console.log(answer);
-          console.log(index);
+          //console.log(answer);
+          //console.log(index);
         }
+        this.formToEmail= ()=>{
+          const body= [];
+          for(let item of this.state.data){
+
+            if(item.h1 !==undefined && item.h2 === undefined && this.state.output[item.id]===undefined){
+            body.push(
+              item.h1+ "\n"
+            );
+            }
+            else if(item.h1 !==undefined && item.h2 === undefined){
+            body.push(
+              item.h1+" " + this.state.output[item.id] + "\n"
+            );
+            }
+            else if (item.h1 ===undefined && item.h2 !== undefined){
+            body.push(
+              item.h2+" " + this.state.output[item.id] + "\n"
+            );
+            }
+            else{
+              body.push(
+                item.h1 +" "+item.h2 +" "+ this.state.output[item.id] + "\n"
+              );
+            }
+          }
+          this.handleEmail(" "," ",body);
+
+        }
+        this.handleEmail = (subject,recipient,body) => {
+          const to = recipient // string or array of email addresses
+          email(to, {
+            subject: this.subject,
+            body: body.toString()
+          }).catch(console.error)
+          console.log(body.toString());
+        }
+
     }
 
   render() {
     return (
       //<ScrollView >
-        <View>
-          {this.renderform()}
-       </View>
+      <View>
+        {this.renderform()}
+        <Button
+          title="Submit"
+          onPress={() => this.formToEmail()}
+        />
+      </View>
       //</ScrollView>
    );
   }
@@ -34,17 +78,18 @@ export default class Form extends Component{
 
   renderform() {
 
-    console.log("re rendered");
+    //console.log("re rendered");
     const items = [];
     for (let item of this.state.data) {
-        console.log(this.state.output[item.id]);
+        //console.log(this.state.output[item.id]);
         if(item.input === "Yes/No entry")
           items.push(
             <View key = {item.id}>
               <Text style ={styles.heading1}> {item.h1} </Text>
               <Text style ={styles.heading2}> {item.h2} </Text>
-              <Picker selectedValue = {this.state[item.id]}
+              <Picker selectedValue = {this.state.output[item.id]}
                 onValueChange={(itemValue, itemIndex) => this.updateAnswer(itemValue,item.id)}>
+                    <Picker.Item label = " " value = " "  />
                     <Picker.Item label = "Yes" value = "yes" />
                     <Picker.Item label = "No" value = "no" />
                     <Picker.Item label = "N/A" value = "n/a" />
--- crisis-support-app/screens/FormScreen.js
+++ crisis-support-app/screens/FormScreen.js
@@ -3,10 +3,15 @@ import {View,ScrollView,Text,StyleSheet} from 'react-native';
 import {Content, Card, CardItem, Body} from 'native-base';
 import Form from '../components/atoms/Form';
 //import JsonFormData from './JsonFormData';
-import form0 from  '../assets/Json Files/Memorandum of understanding.json';
-import form1 from  '../assets/Json Files/Implementation Checklist.json';
-import form2 from  '../assets/Json Files/Information Sharing.json';
+
+import form1 from  '../assets/Json Files/Memorandum of understanding.json';
+import form2 from  '../assets/Json Files/Implementation Checklist.json';
 import form3 from  '../assets/Json Files/Information Needs Assessment.json';
+import form4 from  '../assets/Json Files/Identifying The Location.json';
+import form5 from  '../assets/Json Files/KioskOutreach Plan.json';
+import form7 from  '../assets/Json Files/Code of Conduct Guidance.json';
+import form16 from  '../assets/Json Files/Safe Recruitment Checklist.json';
+
 
 import { Colors } from '_constants';
 
@@ -15,29 +20,45 @@ export default class JsonForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      form0: form0,
       form1: form1,
       form2: form2,
-      form3: form3};
+      form3: form3,
+      form4: form4,
+      form5: form5,
+      form7: form7,
+      form16:form16
+    };
   }
     render() {
       return(
           <ScrollView >
           <View>
-            <Text style = {styles.container}>Memorandum of understanding</Text>
-              <Form data = {this.state.form0}/>
+            <Text style = {styles.container}>1. Memorandum of understanding</Text>
+              <Form data = {this.state.form1} title = "Memorandum of understanding"/>
           </View>
             <View>
-              <Text style = {styles.container}>Implementation Checklist</Text>
-                <Form data = {this.state.form1}/>
+              <Text style = {styles.container}>2. Implementation Checklist</Text>
+                <Form data = {this.state.form2} title = "Implementation Checklist"/>
             </View>
             <View>
-               <Text style = {styles.container}>Information Sharing</Text>
-                <Form data = {this.state.form2}/>
+               <Text style = {styles.container}>3. Information Needs Assessment</Text>
+                <Form data = {this.state.form3} title = "Information Needs Assessment"/>
             </View>
             <View>
-               <Text style = {styles.container}>Information Needs Assessment</Text>
-                <Form data = {this.state.form3}/>
+               <Text style = {styles.container}>4. Identifying the Location</Text>
+                <Form data = {this.state.form4} title = "Identifying the Location"/>
+            </View>
+            <View>
+               <Text style = {styles.container}>5. Kiosk/Outreach Plan</Text>
+                <Form data = {this.state.form5}/>
+            </View>
+            <View>
+               <Text style = {styles.container}>7. Code of Conduct Guidance</Text>
+                <Form data = {this.state.form7}/>
+            </View>
+            <View>
+               <Text style = {styles.container}>16. Safe Recruitment Checklist</Text>
+                <Form data = {this.state.form16}/>
             </View>
           </ScrollView>
         );
@@ -49,6 +70,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 10,
-    backgroundColor:Colors.jsonFormBackgroundColor,
+    backgroundColor:Colors.trocaireBlue,
+    color: '#FFFFFF',
+    fontSize: 16,
   },
+
 });
