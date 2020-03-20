89e0145abf3484a9a4a2ccfe7ce36233ef318db9
BrowneDaniel <dbrowne2@tcd.ie>
2020-03-20 18:59:51 +0000

    added actual links to Trocaire sites, pages and reports to links page

--- crisis-support-app/screens/LinksScreen.js
+++ crisis-support-app/screens/LinksScreen.js
@@ -1,15 +1,59 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import {Platform, ScrollView, StyleSheet, Linking, Button, View, TouchableOpacity, Image} from 'react-native';
+import { NavigationButton } from '_atoms';
 import { ExpoLinksView } from '@expo/samples';
 
-export default function LinksScreen() {
+export default function LinksScreen({ navigation }) {
   return (
     <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
+      <View style={styles.rectangularButton}>
+        <View>
+         <NavigationButton
+          title="Trocaire Home"
+          navigationOption={'https://www.trocaire.org'}
+          navigateFunction={Linking.openURL}/>
+        </View>
+      </View>
+      <View style={styles.rectangularButton}>
+        <View>
+         <NavigationButton
+          title="Trocaire News"
+          navigationOption={'https://www.trocaire.org/news'}
+          navigateFunction={Linking.openURL}/>
+        </View>
+      </View>
+      <View style={styles.rectangularButton}>
+        <View>
+         <NavigationButton
+          title="Reports about Trocaire"
+          navigationOption={'https://www.google.com/search?q=trocaire&tbm=nws'}
+          navigateFunction={Linking.openURL}/>
+        </View>
+      </View>
+      <View style={styles.rectangularButton}>
+        <View>
+         <NavigationButton
+          title="Twitter"
+          navigationOption={'https://twitter.com/trocaire'}
+          navigateFunction={Linking.openURL}/>
+        </View>
+      </View>
+      <View style={styles.rectangularButton}>
+        <View>
+         <NavigationButton
+          title="Facebook"
+          navigationOption={'https://www.facebook.com/trocaireireland/'}
+          navigateFunction={Linking.openURL}/>
+        </View>
+      </View>
+      <View style={styles.rectangularButton}>
+        <View>
+         <NavigationButton
+          title="Instagram"
+          navigationOption={'https://www.instagram.com/trocaireonline/'}
+          navigateFunction={Linking.openURL}/>
+        </View>
+      </View>
     </ScrollView>
   );
 }
@@ -21,7 +65,40 @@ LinksScreen.navigationOptions = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: 'white',
+    paddingTop: '5%'
+  },
+  rectangularButton: {
+    paddingLeft: '40%',
+    paddingRight: '40%',
+    paddingTop: '.5%',
+    paddingBottom: '.5%',
+    alignContent: 'center',
+
+    ...Platform.select({
+      android: {
+        paddingLeft: '25%',
+        paddingRight: '25%',
+        paddingTop: '4%',
+        paddingBottom: '4%',
+      },
+    }),
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    paddingLeft: '20%',
+    paddingRight: '20%',
+    height: '30%',
+    marginBottom: '-5%',
+    marginTop: '5%',
+    ...Platform.select({
+      android: {
+        paddingLeft: '20%',
+        paddingRight: '20%',
+        height: '30%',
+        marginBottom: '-30%',
+        marginTop: '25%',
+      },
+    }),
   },
 });
